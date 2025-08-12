459aa42695db120eec513a483ac9a8aec1063518
Jeremias Pfluger <jerepfluger@gmail.com>
2025-08-12 13:19:38 +0200

    proper workflow cron + all telegram messages

--- api.py
+++ api.py
@@ -2,4 +2,5 @@ from service.availability_service import SpanishCitizenshipService
 
 if __name__ == '__main__':
     spanish_citizenship_service = SpanishCitizenshipService()
+
     spanish_citizenship_service.check_appointment_availability()
